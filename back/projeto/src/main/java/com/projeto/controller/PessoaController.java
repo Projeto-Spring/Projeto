309502f562b1cd5ef3c271a81d8d27fd4ca69038
PedrohFolster <pedro.santos13@alunos.sc.senac.br>
2024-04-15 20:10:06 -0300

    Validar Senha

--- back/projeto/src/main/java/com/projeto/controller/PessoaController.java
+++ back/projeto/src/main/java/com/projeto/controller/PessoaController.java
@@ -6,12 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.projeto.model.Pessoa;
-import com.projeto.Util.ValidaSenha;
+
 import com.projeto.repository.JDBC.PessoaRepositoryJDBC;
-import com.projeto.repository.PessoaRepository;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PessoaController {
@@ -22,9 +21,11 @@ public class PessoaController {
     }
 
     @PostMapping("/login")
-    public String login(@RequestParam String cpf, Model model) {
+    public String login(@RequestParam String cpf, HttpSession session, Model model) {
         // Verifica se o CPF existe no banco de dados
         if (pessoaRepository.verificarCpf(cpf)) {
+            // Armazena o CPF na sessão do usuário
+            session.setAttribute("cpf", cpf);
             // Verifica se a senha está definida para o CPF
             String senha = pessoaRepository.buscarSenhaPorCpf(cpf);
             if (senha != null) {
@@ -42,27 +43,26 @@ public class PessoaController {
             return "login";
         }
     }
-    
-    
 
     @GetMapping("/criarSenha")
     public String showCreatePasswordPage(@RequestParam String cpf, Model model) {
         model.addAttribute("cpf", cpf);
-        return "criarSenha"; // Retornar a página de criação de senha
+        return "criarSenha";
     }
 
     @PostMapping("/atualizarSenha")
-    public String atualizarSenha(@RequestParam("cpf") String cpf, @RequestParam("novaSenha") String novaSenha) {
-        pessoaRepository.update(cpf, novaSenha);
-        return "redirect:/home";
+    public String atualizarSenha(@RequestParam("novaSenha") String novaSenha, HttpSession session) {
+        String cpf = (String) session.getAttribute("cpf");
+        if (cpf != null) {
+            pessoaRepository.atualizarSenhaPorCpf(cpf, novaSenha);
+            return "redirect:/home";
+        } else {
+            return "redirect:/pagina_de_erro";
+        }
     }
-    
-
 
     @GetMapping("/home")
     public String homePage(Model model) {
-        // Adicione aqui lógica adicional, se necessário
-        return "home"; // Retorna o nome da página HTML para exibir
+        return "home";
     }
-
 }
--- back/projeto/src/main/java/com/projeto/repository/JDBC/PessoaRepositoryJDBC.java
+++ back/projeto/src/main/java/com/projeto/repository/JDBC/PessoaRepositoryJDBC.java
@@ -1,9 +1,7 @@
 package com.projeto.repository.JDBC;
 
 import com.projeto.model.Pessoa;
-
 import java.util.List;
-
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -23,12 +21,9 @@ public class PessoaRepositoryJDBC implements PessoaRepository{
 
     }
     
-
-
     @Override
     public List<Pessoa> findAll() {
         return null;
-
     }
 
     @Override
@@ -36,8 +31,6 @@ public class PessoaRepositoryJDBC implements PessoaRepository{
         String sql = "UPDATE pessoa SET senha = ? WHERE cpf = ?";
         jdbcTemplate.update(sql, novaSenha, cpf);
     }
-    
-    
 
     @Override
     public void delete(int idPessoa) {
@@ -46,23 +39,18 @@ public class PessoaRepositoryJDBC implements PessoaRepository{
 
     @Override
     public boolean verificarCpf(String cpf) {
-        // Remover pontos, traços e outros caracteres não numéricos do CPF
         String cpfSemFormatacao = cpf.replaceAll("[^0-9]", "");
-    
         String sql = "SELECT COUNT(*) FROM pessoa WHERE REPLACE(REPLACE(cpf, '.', ''), '-', '') = ?";
         int count = jdbcTemplate.queryForObject(sql, Integer.class, cpfSemFormatacao);
         return count > 0;
     }
-    
 
     @Override
     public String buscarSenhaPorCpf(String cpf) {
         String sql = "SELECT senha FROM pessoa WHERE cpf = ?";
         try {
-            // Consulta o banco de dados para obter a senha associada ao CPF fornecido
             return jdbcTemplate.queryForObject(sql, String.class, cpf);
         } catch (EmptyResultDataAccessException e) {
-            // Retorna null se não houver senha associada ao CPF
             return null;
         }
     }
