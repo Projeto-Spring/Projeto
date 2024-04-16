8b8db92fb2ecbc92f890343752336a3fb2c2fc51
Vicente Pereira <vicentepereira9486@gmail.com>
2024-04-15 21:40:18 -0300

    corrigindo warning
    corrigindo warning

--- back/projeto/src/main/java/com/projeto/controller/PessoaController.java
+++ back/projeto/src/main/java/com/projeto/controller/PessoaController.java
@@ -1,7 +1,5 @@
 package com.projeto.controller;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
