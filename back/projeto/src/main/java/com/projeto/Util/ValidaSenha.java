cbaac10eefb5c31e0cbcd5a069066b90b26cc41d
Gabriel Becker <gabrielbeckeandrade@gmail.com>
2024-04-21 13:47:33 -0300

    Update ValidaSenha.java

--- back/projeto/src/main/java/com/projeto/Util/ValidaSenha.java
+++ back/projeto/src/main/java/com/projeto/Util/ValidaSenha.java
@@ -2,4 +2,12 @@ package com.projeto.Util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
+public class ValidaSenha {
+    public static boolean validaSenha(String senha) {
+        // Regex para validar a senha
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,16}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(senha);
+        return matcher.matches();
+    }
+}
